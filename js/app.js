ffb70cfbc0e0eda2f9dd6a387b16da9aef693d1a
Peter Vu <me@petervu.ca>
2016-06-08 22:45:44 -0400

    Added table filters
    Added checkboxes for module select and as well added new feature to
    select and deselect all module checkboxes of entity. Added search bar
    filter also. These all can be used in conjunction with each other. Next
    up will be hide all reg type 3s and then fix up the comment rendering.

--- js/app.js
+++ js/app.js
@@ -29,6 +29,8 @@ app.controller("posdaCtrl", function ($scope, $http) {
     var filename;
     var iodData;
     $scope.tableData = [];
+    $scope.entities = [];
+    $scope.modules = [];
     for(i=0;i<$scope.dataSelect.table.length;i++){
       if ($scope.tableSelect == $scope.dataSelect.table[i].Title){
         filename = $scope.dataSelect.table[i].Name;
@@ -39,17 +41,70 @@ app.controller("posdaCtrl", function ($scope, $http) {
     var filepath = $scope.dataSelect.path + $scope.dataSelect.prefix + filename + $scope.dataSelect.suffix;
     require([filepath], function(){
       iodData = data.tags;
+
+      //Test tabulate
       for(var dataRow in iodData){
-      var dataRowMod = iodData[dataRow];
-      dataRowMod['element'] = dataRow;
-      $scope.tableData.push(dataRowMod);
-   }
+        var dataRowMod = iodData[dataRow];
+        dataRowMod['element'] = dataRow;
+        $scope.tableData.push(dataRowMod);
+
+          //Entity+module rendering starts here
+            var entityExists = false;
+            for (i=0;i<$scope.entities.length;i++){
+              if (dataRowMod.entity == $scope.entities[i]){
+                entityExists = true;
+                var moduleExists = false;
+                for (n=0;n<$scope.modules[i].length;n++){
+                  if (dataRowMod.module == $scope.modules[i][n].name){
+                    moduleExists = true;
+                    break;
+                  }
+                }
+                if (!moduleExists && dataRowMod.module != undefined){
+                  $scope.modules[i].push({name:dataRowMod.module,selected:true});
+                }
+                break;
+              }
+            }
+            if(!entityExists && dataRowMod.entity != undefined){
+              $scope.entities.push(dataRowMod.entity);
+              $scope.modules.push([]);
+              $scope.modules[$scope.entities.length-1].push({name:dataRowMod.module,selected:true});
+            }
+          }
+
    $scope.tableSelected = true;
+   $scope.filterModule();
    $scope.$apply();
     });
   }
 
   $scope.changeOrderBy = function(x) {
     $scope.orderByThis = x;
+  }
+
+  $scope.filterEntity = function(p){
+    for(i=0;i<$scope.modules[p].length;i++){
+      $scope.modules[p][i].selected = !$scope.modules[p][i].selected;
+    }
+    $scope.filterModule();
+  }
+
+  $scope.filterModule = function(){
+    $scope.moduleFilter = [];
+    for(i=0;i<$scope.modules.length;i++){
+      for(n=0;n<$scope.modules[i].length;n++){
+        if($scope.modules[i][n].selected == true){
+          $scope.moduleFilter.push($scope.modules[i][n].name);
+        }
+      }
+    }
+  }
+
+  $scope.modulesChecked = function(row){
+    if ($.inArray(row.module, $scope.moduleFilter) < 0){
+      return;
+    }
+    return row;
   }
 });
