4b0ccefdf6eba9dad106d54835d8b66123bd47ff
Peter Vu <petervu@live.ca>
2016-07-17 00:31:59 -0400

    Minor Parse Script Update
    Stricter argument acceptor. Script no longer async so removed timer.

--- tools/htmlparse/linkanchorparse.js
+++ tools/htmlparse/linkanchorparse.js
@@ -26,7 +26,7 @@ var parseHTMLTitles = function(directory,i){
 
               if(anchorLink !== undefined){
                 titleOutput.push({name: titleName, anchor: anchorLink, filepath: itemPath});
-                var currentTime = new Date().getTime()
+                var currentTime = new Date().getTime();
                 var fileTime = currentTime - lastTime;
                 console.log(itemPath + " | " + (fileTime) + "ms");
                 lastTime = currentTime;
@@ -37,24 +37,23 @@ var parseHTMLTitles = function(directory,i){
 };
 
 var directoryInput = process.argv[2];
-console.log(directoryInput);
-var outputFile = process.argv.slice(3);
-if(outputFile === undefined){
-  outputFile = "xreflinks.json";
+var outputFile = process.argv[3];
+if(outputFile === undefined || directoryInput === undefined){
+  console.log("The program accepts arguments:");
+  console.log("node linkanchorparse.js <input directory> <outputfilepath>");
+} else {
+	parseHTMLTitles(directoryInput);
+
+	var jsonTitleOutput = JSON.stringify(titleOutput);
+
+	fs.writeFile(outputFile, jsonTitleOutput, function(err) {
+	  if(err) {
+		  return console.log(err);
+	  }
+
+	  console.log("The file was saved!");
+	  var end = new Date().getTime();
+	  var elapsed = end-start;
+	  console.log("Operation took: " + elapsed + "ms");
+	});
 }
-parseHTMLTitles(directoryInput);
-
-var jsonTitleOutput = JSON.stringify(titleOutput);
-
-setTimeout( function() {
-  fs.writeFile("HTMLTitleParse.json", jsonTitleOutput, function(err) {
-      if(err) {
-          return console.log(err);
-      }
-
-      console.log("The file was saved!");
-      var end = new Date().getTime();
-      var elapsed = end-start;
-      console.log("Operation took: " + elapsed + "ms");
-  });
-}, 2000 );
